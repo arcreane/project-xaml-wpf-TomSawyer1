f275436f1f0f2a1999d61d1045425024a12643f1
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2024-05-24 00:24:05 +0200

    maui

---
+++ MauiApp_Dessiner/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp_Dessiner;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiApp_Dessiner/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp_Dessiner;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
